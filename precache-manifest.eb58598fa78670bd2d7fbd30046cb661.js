2afc31252d9e4c19b08996368f67959befd058ca
AndIReyf <andrey.prudius@hotmail.com>
2020-09-03 15:46:02 +0200

    Updates

---
+++ precache-manifest.eb58598fa78670bd2d7fbd30046cb661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819b33fdfd54db0ac753c5589285d7e0",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "ca645fbc2420956ac71a",
+    "url": "/TodoList/static/css/main.5f740986.chunk.css"
+  },
+  {
+    "revision": "15dc3a0ee07362655866",
+    "url": "/TodoList/static/js/2.9a365b4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList/static/js/2.9a365b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca645fbc2420956ac71a",
+    "url": "/TodoList/static/js/main.0793d176.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
